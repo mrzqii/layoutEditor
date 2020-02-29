395c52ff7ff830a8465e9f56fbf8fc6166cea9d2
zengqih <>
2020-02-29 17:10:55 +0800

    first

---
+++ precache-manifest.07a0a07aec1fa6a7d3441458bd79bd67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc1e0f96f250a5eb76c355cfb8cea92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a89fa87507ab36936659",
+    "url": "/static/css/2.faecdb03.chunk.css"
+  },
+  {
+    "revision": "741d9ea7c3721445035b",
+    "url": "/static/css/main.054ef1bd.chunk.css"
+  },
+  {
+    "revision": "a89fa87507ab36936659",
+    "url": "/static/js/2.9eb59d83.chunk.js"
+  },
+  {
+    "revision": "8586b89c81a042f4d711f03f1a4938ab",
+    "url": "/static/js/2.9eb59d83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "741d9ea7c3721445035b",
+    "url": "/static/js/main.772eb164.chunk.js"
+  },
+  {
+    "revision": "f87f191c733ec2bba81f",
+    "url": "/static/js/runtime-main.77653299.js"
+  }
+]);
